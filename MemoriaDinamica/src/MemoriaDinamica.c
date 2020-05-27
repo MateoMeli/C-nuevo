c9fff0e399ff4c3d4ce2a6051a1bd577901ad992
mateomeli <mateomeli7@gmail.com>
2020-05-27 20:41:01 -0300

    Actualizado 27/5

--- MemoriaDinamica/src/MemoriaDinamica.c
+++ MemoriaDinamica/src/MemoriaDinamica.c
@@ -10,24 +10,44 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
+#define TAM 4
 
 
 int main(void) {
 
 	setbuf(stdout, NULL);
 
+	int tam = TAM;
 	int* a;
+	int* aux;
 	int i;
-	a = (int*) malloc(sizeof(int)*20);
+	a = (int*) malloc(sizeof(int)*TAM);
 
 	if(a != NULL)
 	{
-		for(i = 0; i < 20; i ++)
+		for(i = 0; i < TAM; i ++)
 		{
 			*(a+i) = 0;
 		}
 	}
+
+	aux = (int*) realloc(a, (TAM + 4) * sizeof(int));
+
+	if(aux != NULL)
+	{
+		tam = TAM + 4;
+		a = aux;
+	}else{
+		printf("No queda memoria.\n");
+	}
+
+
+	for(i = 0; i < tam; i ++)
+	{
+		printf("%d\n", *(a+i));
+	}
+
+	free(a);
 
 	return EXIT_SUCCESS;
 }
